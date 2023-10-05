72b8ac11254226ec7b50a7a79473ee50bb622c2c
John Hoareau <hxjwork1@gmail.com>
2023-10-05 14:36:26 +0400

    Read file content from path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let configuration_path: &str = "./tests/configurations.json";
+    let configuration_content: String = read_configuration_file(configuration_path);
+
+    println!("{}", &configuration_content);
+}
+
+fn read_configuration_file(configuration_file_path: &str) -> String {
+    let error_message: String = format!("Cannot read configuration file from path {0}",
+                                configuration_file_path);
+
+    fs::read_to_string(configuration_file_path)
+        .expect(&error_message)
 }
