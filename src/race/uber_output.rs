0ef4fdc1eb44aaed555637c66a4b236ecb7ecbdd
John Hoareau <hxjwork1@gmail.com>
2023-10-08 21:56:24 +0400

    #17: do something (println for now) when event is consumed

--- src/race/uber_output.rs
+++ src/race/uber_output.rs
@@ -15,10 +15,26 @@ pub enum UberOutput {
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-enum UberOutputSftpAuthenticationMethod {
+pub enum UberOutputSftpAuthenticationMethod {
     Password(String),
     Key {
         #[serde(rename = "publicKeyPath")] public_key_path: String,
         #[serde(rename = "privateKeyPath")] private_key_path: String
+    }
+}
+
+impl UberOutput {
+
+    pub fn take_passenger_and_drive_to(&self, input_file_path: &str) {
+        match &self {
+            UberOutput::Local { path } => {
+                println!("Should copy file locally from {} to {}", input_file_path, path);
+            }
+
+            UberOutput::Sftp { login, authentication_method, remote_path } => {
+                println!("Should copy file remotely from {} to {} using login {} with auth method {:?}",
+                         input_file_path, remote_path, login, authentication_method);
+            }
+        }
     }
 }
