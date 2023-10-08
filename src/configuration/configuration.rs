ab1d90237299c80b428153c116e74f4c5e7b3567
John Hoareau <hxjwork1@gmail.com>
2023-10-08 11:43:55 +0400

    #8: review models

--- src/configuration/configuration.rs
+++ src/configuration/configuration.rs
@@ -1,31 +1,49 @@
 use serde::{Serialize, Deserialize};
 use serde_json::{from_str as serde_json_from_str, to_string as serde_json_to_string};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Configuration {
-    pub input: ConfigurationInput,
-    pub outputs: Vec<ConfigurationOutput>
+#[derive(Deserialize, Debug)]
+pub struct Races {
+    pub ubers: Vec<Uber>
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct ConfigurationInput {
+pub struct Uber {
+    pub input: UberInput,
+    pub outputs: Vec<UberOutput>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UberInput {
     pub path: String
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct ConfigurationOutput {
+pub struct UberOutput {
     pub path: String
 }
 
-impl Configuration {
+impl Races {
 
     pub fn to_string(&self) -> String {
         serde_json_to_string(self)
-            .expect("Cannot serialize configuration")
+            .expect("Cannot serialize races")
     }
 
     pub fn from_string(content: &str) -> Self {
         serde_json_from_str(content)
-            .expect("Cannot deserialize configuration")
+            .expect("Cannot deserialize races")
+    }
+}
+
+impl Uber {
+
+    pub fn to_string(&self) -> String {
+        serde_json_to_string(self)
+            .expect("Cannot serialize uber")
+    }
+
+    pub fn from_string(content: &str) -> Self {
+        serde_json_from_str(content)
+            .expect("Cannot deserialize uber")
     }
 }
