ee4f9f821c6efee267cd6881d5840f6939827c49
John Hoareau <hxjwork1@gmail.com>
2023-10-08 14:12:23 +0400

    #10: use config instead of string

--- src/event/event_consumer.rs
+++ src/event/event_consumer.rs
@@ -1,15 +1,14 @@
 use kafka::consumer::{Consumer, FetchOffset};
 
+use crate::configuration::configuration::ConfigKafka;
 use crate::race::uber::Uber;
 
-pub fn consume_uber() {
-    let hosts: Vec<String> = vec!["localhost:9092".to_owned()];
-
-    let mut consumer: Consumer = Consumer::from_hosts(hosts)
-            .with_topic("uber-race".to_owned())
-            .with_fallback_offset(FetchOffset::Latest)
-            .create()
-            .expect("Cannot create uber consumer");
+pub fn consume_uber(kafka_config: &ConfigKafka) {
+    let mut consumer: Consumer = Consumer::from_hosts(kafka_config.hosts.to_owned())
+        .with_topic(kafka_config.topic.to_owned())
+        .with_fallback_offset(FetchOffset::Latest)
+        .create()
+        .expect("Cannot create uber consumer");
 
     loop {
         for ms in consumer.poll().unwrap().iter() {
