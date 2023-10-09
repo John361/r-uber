3d72a7e0d1e6a6c5aaa4b96c4e458308d60c1c06
John Hoareau <hxjwork1@gmail.com>
2023-10-09 16:07:51 +0400

    #25: improve event_uber

--- src/event/event_uber.rs
+++ src/event/event_uber.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use serde_json::{from_str as serde_json_from_str, to_string as serde_json_to_string};
 
+use crate::logger;
 use crate::race::uber::Uber;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -10,11 +11,27 @@ pub struct EventUber {
 }
 
 impl EventUber {
-    pub fn to_json_string(&self) -> String {
-        serde_json_to_string(self).expect("Cannot serialize uber")
+    pub fn to_json_string(&self) -> Result<String, String> {
+        match serde_json_to_string(self) {
+            Ok(content) => { Ok(content) }
+
+            Err(error) => {
+                let error_message = format!("Cannot convert EventUber to String: {}", error);
+                logger::warn("uber_event", "to_json_string", &error_message);
+                Err(error_message)
+            }
+        }
     }
 
-    pub fn from_json_string(content: &str) -> Self {
-        serde_json_from_str(content).expect("Cannot deserialize uber")
+    pub fn from_json_string(content: &str) -> Result<Self, String> {
+        match serde_json_from_str::<EventUber>(content) {
+            Ok(event_uber) => { Ok(event_uber) }
+
+            Err(error) => {
+                let error_message = format!("Cannot convert from string to EventUber: {}", error);
+                logger::warn("uber_event", "from_json_string", &error_message);
+                Err(error_message)
+            }
+        }
     }
 }
