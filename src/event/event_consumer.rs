e9a51d06f37c622b911440a9240b77e11cc8604e
John Hoareau <hxjwork1@gmail.com>
2023-10-09 16:52:21 +0400

    #25: improve event_consumer

--- src/event/event_consumer.rs
+++ src/event/event_consumer.rs
@@ -2,28 +2,52 @@ use kafka::consumer::{Consumer, FetchOffset};
 
 use crate::configuration::ConfigKafka;
 use crate::event::event_uber::EventUber;
+use crate::logger;
 
 pub fn consume_uber(kafka_config: &ConfigKafka) {
-    let mut consumer: Consumer = Consumer::from_hosts(kafka_config.hosts.to_owned())
+    let client_consumer = Consumer::from_hosts(kafka_config.hosts.to_owned())
         .with_topic(kafka_config.topic.to_owned())
         .with_fallback_offset(FetchOffset::Latest)
-        .create()
-        .expect("Cannot create uber consumer");
+        .create();
 
-    loop {
-        for ms in consumer.poll().unwrap().iter() {
-            for m in ms.messages() {
-                let content: &str = std::str::from_utf8(m.value).unwrap();
-                let message: EventUber = EventUber::from_json_string(content);
+    match client_consumer {
+        Ok(mut consumer) => {
+            loop {
+                for ms in consumer.poll().unwrap().iter() {
+                    for m in ms.messages() {
+                        match std::str::from_utf8(m.value) {
+                            Ok(content) => {
+                                match EventUber::from_json_string(content) {
+                                    Ok(message) => {
+                                        for uber_output in &message.uber.outputs {
+                                            uber_output.take_passenger_and_drive_to(&message.uber, &message.passenger);
+                                        }
+                                    }
 
-                for uber_output in &message.uber.outputs {
-                    uber_output.take_passenger_and_drive_to(&message.uber, &message.passenger);
+                                    Err(error) => {
+                                        let error_message: String = format!("Cannot convert uber event message: {}", error);
+                                        logger::error("event_consumer", "consume_uber", &error_message);
+                                    }
+                                }
+                            }
+
+                            Err(error) => {
+                                let error_message: String = format!("Cannot read uber event message: {}", error);
+                                logger::error("event_consumer", "consume_uber", &error_message);
+                            }
+                        }
+                    }
+
+                    consumer.consume_messageset(ms).unwrap();
                 }
-            }
 
-            consumer.consume_messageset(ms).unwrap();
+                consumer.commit_consumed().unwrap_or_default();
+            }
         }
 
-        consumer.commit_consumed().unwrap_or_default();
+        Err(error) => {
+            let error_message: String = format!("Cannot create uber consumer: {}", error);
+            logger::error("event_consumer", "consume_uber", &error_message);
+        }
     }
 }
