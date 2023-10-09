f5e3e5a49d99d2f4902ff514d2a295b51419c1be
John Hoareau <hxjwork1@gmail.com>
2023-10-09 15:01:53 +0400

    #25: improve local

--- src/race_action/local.rs
+++ src/race_action/local.rs
@@ -1,21 +1,47 @@
 use std::path::Path;
 
+use crate::logger;
 use crate::race::uber_output::UberOutput;
 
-pub fn copy(input: &Path, output: &UberOutput) -> std::io::Result<u64> {
+pub fn copy(input: &Path, output: &UberOutput) -> Result<bool, String> {
     match output {
         UberOutput::Local { path } => {
-            let passenger_name = input
-                .file_name()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .replace('"', "");
-            let full_destination_path = format!("{}/{}", path, passenger_name);
+            match input.file_name() {
+                Some(file_name) => {
+                    match file_name.to_str() {
+                        Some(name) => {
+                            let passenger_name: String = name.replace('"', "");
+                            let full_destination_path: String = format!("{}/{}", path, passenger_name);
 
-            std::fs::copy(input, full_destination_path)
+                            if let Err(error) = std::fs::copy(input, &full_destination_path) {
+                                let error_message: String = format!("Cannot copy file {:?} to {}: {}", input, &full_destination_path, error);
+                                logger::warn("local", "copy", &error_message);
+                                Err(error_message)
+                            } else {
+                                let success_message: String = format!("Successfully copy file from {:?} to {}", input, &full_destination_path);
+                                logger::info("local", "copy", &success_message);
+                                Ok(true)
+                            }
+                        }
+
+                        None => {
+                            logger::warn("local", "copy", "Cannot get file name as string");
+                            Err("Cannot get file name as string".to_string())
+                        }
+                    }
+                }
+
+                None => {
+                    logger::warn("local", "copy", "Cannot get file name");
+                    Err("Cannot get file name".to_string())
+                }
+            }
         }
 
-        _ => Ok(0),
+        _ => {
+            let error_message: String = format!("Wrong output used with the method: {:?}", output);
+            logger::warn("local", "copy", &error_message);
+            Err(error_message)
+        },
     }
 }
