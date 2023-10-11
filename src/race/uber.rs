38330f25ea242a37d8284f8ed6536a2406c2136b
John Hoareau <hxjwork1@gmail.com>
2023-10-11 20:26:46 +0400

    #35: create method and use it

--- src/race/uber.rs
+++ src/race/uber.rs
@@ -1,10 +1,24 @@
 use serde::{Deserialize, Serialize};
 
+use crate::logger;
 use crate::race::uber_input::UberInput;
 use crate::race::uber_output::UberOutput;
+use crate::race_action::local;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Uber {
     pub input: UberInput,
     pub outputs: Vec<UberOutput>,
+}
+
+impl Uber {
+    pub fn kick_passenger(&self, passenger: &str) {
+        if let Err(error) = local::delete(passenger.as_ref()) {
+            let error_message = format!("Error occurred when trying to kick passenger: {}", error);
+            logger::warn("uber", "kick_passenger", &error_message);
+        } else {
+            let success_message = format!("Successfully kick passenger {}", passenger);
+            logger::info("uber", "kick_passenger", &success_message);
+        }
+    }
 }
